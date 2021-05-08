2ca2ff2113e110e66d70c2418c3f800850d450a1
Shraboni Chakraborty <shrabonichakraborty67@gmail.com>
2021-05-08 10:25:49 +0530

    remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,3 @@
-//const { response } = require("express")
-
-console.log('this is a client side javascript')
-
 const weatherform = document.querySelector('form')
 const search = document.querySelector('input')
 const messageone = document.querySelector('#message-1')
